b1120237239d9de5acb8af77cdf05f91a86eaa53
yann <yann.hortance@gmail.com>
2020-04-25 13:52:07 +0200

    Add error endpoint

--- userMicroservice/userService/src/main/java/com/yann/myebaylike/user_service/ApplicationConfiguration.java
+++ userMicroservice/userService/src/main/java/com/yann/myebaylike/user_service/ApplicationConfiguration.java
@@ -4,11 +4,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @Configuration
 public class ApplicationConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final AuthenticationFailureHandler defaultAuthenticationFailureHandler = new AuthenticationFailureHandler() {
+        @Override
+        public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e)
+                throws IOException, ServletException {
+            httpServletResponse.getWriter().append("Authentication failure");
+            httpServletResponse.setStatus(401);
+        }
+    };
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -26,7 +42,12 @@ public class ApplicationConfiguration extends WebSecurityConfigurerAdapter {
             .csrf(c -> c
                     .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
             )
-            .oauth2Login();
+            .oauth2Login(o -> o
+                    .failureHandler((request, response, exception) -> {
+                        request.getSession().setAttribute("error.message", exception.getMessage());
+                        defaultAuthenticationFailureHandler.onAuthenticationFailure(request, response, exception);
+                    })
+            );
         //http.requiresChannel().anyRequest().requiresSecure();
     }
 }
---
+++ userMicroservice/userService/src/main/java/com/yann/myebaylike/user_service/controllers/ErrorController.java
@@ -0,0 +1,18 @@
+package com.yann.myebaylike.user_service.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RequestMapping
+@RestController
+public class ErrorController {
+    @GetMapping("/error")
+    public String error(HttpServletRequest request) {
+        String message = (String) request.getSession().getAttribute("error.message");
+        request.getSession().removeAttribute("error.message");
+        return message;
+    }
+}
