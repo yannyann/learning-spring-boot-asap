cf8ff0d23c3b7a5b91a73ec5c1c68d380de653e9
yann <yann.hortance@gmail.com>
2020-04-25 11:45:33 +0200

    Restructure projects

---
+++ userMicroservice/userService/src/test/java/com/yann/myebaylike/MyebaylikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yann.myebaylike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyebaylikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
